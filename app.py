985e63f44f248f6ae4dce3a1259f394b0665a595
Aidrin Varghese <aidrinv2001@gmail.com>
2024-04-09 10:34:52 +0530

    feat:main page

--- app.py
+++ app.py
@@ -87,53 +87,53 @@ def user_input(user_question):
     return response
 
 
-# def main():
-#     st.set_page_config(page_title="Gemini PDF Chatbot", page_icon="🤖")
-#     with st.sidebar:
-#         st.title("Menu:")
-#         pdf_docs = st.file_uploader(
-#             "Upload your PDF Files and Click on the Submit & Process Button",
-#             accept_multiple_files=True,
-#         )
-#         if st.button("Submit & Process"):
-#             with st.spinner("Processing..."):
-#                 raw_text = get_pdf_text(pdf_docs)
-#                 text_chunks = get_text_chunks(raw_text)
-#                 get_vector_store(text_chunks)
-#                 st.success("Done")
+def main():
+    st.set_page_config(page_title="Gemini PDF Chatbot", page_icon="🤖")
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader(
+            "Upload your PDF Files and Click on the Submit & Process Button",
+            accept_multiple_files=True,
+        )
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
 
-#     st.title("Chat with PDF files using Gemini🤖")
-#     st.write("Welcome to the chat!")
-#     st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
+    st.title("Chat with PDF files using Gemini🤖")
+    st.write("Welcome to the chat!")
+    st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
 
-#     if "messages" not in st.session_state.keys():
-#         st.session_state.messages = [
-#             {"role": "assistant", "content": "upload some pdfs and ask me a question"}
-#         ]
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "upload some pdfs and ask me a question"}
+        ]
 
-#     for message in st.session_state.messages:
-#         with st.chat_message(message["role"]):
-#             st.write(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-#     if prompt := st.chat_input():
-#         st.session_state.messages.append({"role": "user", "content": prompt})
-#         with st.chat_message("user"):
-#             st.write(prompt)
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
 
-#     if st.session_state.messages[-1]["role"] != "assistant":
-#         with st.chat_message("assistant"):
-#             with st.spinner("Thinking..."):
-#                 response = user_input(prompt)
-#                 placeholder = st.empty()
-#                 full_response = ""
-#                 for item in response["output_text"]:
-#                     full_response += item
-#                     placeholder.markdown(full_response)
-#                 placeholder.markdown(full_response)
-#         if response is not None:
-#             message = {"role": "assistant", "content": full_response}
-#             st.session_state.messages.append(message)
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ""
+                for item in response["output_text"]:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
+        if response is not None:
+            message = {"role": "assistant", "content": full_response}
+            st.session_state.messages.append(message)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
